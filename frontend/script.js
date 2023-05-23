27c60d865845a7c2e43950399e3ba909b67a1c74
Mehul Mohan <mehulmpt@gmail.com>
2023-05-23 07:57:30 +0530

    some changes

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,10 @@
-const fileUploadInput = document.getElementById("file-upload")
-const results = document.getElementById("results")
-const uploadStatusH2 = document.getElementById("upload-status")
-const imageBox = document.getElementById("image-upload-preview")
-const imageBoxIMG = imageBox.querySelector("img")
+const fileUploadInput = document.getElementById('file-upload')
+const results = document.getElementById('results')
+const uploadStatusH2 = document.getElementById('upload-status')
+const imageBox = document.getElementById('image-upload-preview')
+const imageBoxIMG = imageBox.querySelector('img')
 
-fileUploadInput.addEventListener("change", handleFileSelect, false)
+fileUploadInput.addEventListener('change', handleFileSelect, false)
 
 function sleep(duration) {
 	return new Promise((resolve) => {
@@ -22,30 +22,27 @@ async function handleFileSelect(evt) {
 
 	const tempImageSrc = URL.createObjectURL(file)
 
-	imageBox.style.display = "block"
-	imageBox.querySelector("img").setAttribute("src", tempImageSrc)
-	uploadStatusH2.innerText = "Uploading..."
+	imageBox.style.display = 'block'
+	imageBox.querySelector('img').setAttribute('src', tempImageSrc)
+	uploadStatusH2.innerText = 'Uploading...'
 
 	// TODO: Make a backend call to send this image
 
 	// Create a FormData object to send the file in the request body
 	const formData = new FormData()
-	formData.append("file", file)
+	formData.append('file', file)
 
 	// TODO: Show the backend response data here
-	let backendResponse = {
-		diseaseType: "xyz",
-		quality: 0.5,
-		probability: 0.66,
-		name: "XYZ",
-		gender: "female",
-	}
+	let backendResponse = {}
 
 	try {
-		const response = await fetch("http://146.190.112.143:3000/model-output", {
-			method: "POST",
-			body: formData,
-		})
+		const response = await fetch(
+			'http://146.190.112.143:3000/model-output',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
 		const data = await response.json()
 		backendResponse = data
 	} catch (error) {
@@ -54,8 +51,8 @@ async function handleFileSelect(evt) {
 
 	await sleep(2000)
 
-	uploadStatusH2.innerText = ""
-	imageBoxIMG.classList.remove("opacity-40")
+	uploadStatusH2.innerText = ''
+	imageBoxIMG.classList.remove('opacity-40')
 
 	results.innerText = JSON.stringify(backendResponse, null, 4)
 }
